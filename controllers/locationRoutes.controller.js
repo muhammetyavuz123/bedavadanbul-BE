dc2aa9d1aa967fa99ecb6219a52c26d04a19f029
myavuz <muhametyavuz@hotmail.com>
2026-08-01 15:12:58 +0300

    cors fixx

--- controllers/locationRoutes.controller.js
+++ controllers/locationRoutes.controller.js
@@ -1,7 +1,6 @@
 import { data } from "../data/il-ilce.js";
 
 export const getCities = async (req, res) => {
-  console.log("🚀 ~ getCities ~ req:", req);
   const query = req.query;
   try {
     const iller = data.map((il) => ({
